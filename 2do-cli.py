d276096cc6ab5f7fadf275eee6cd0a6abd6c1d6e
Rob Greer <rob.g.greer@gmail.com>
2012-02-26 20:56:15 -0500

    Converted file to positional argument

--- 2do-cli.py
+++ 2do-cli.py
@@ -4,11 +4,11 @@ import argparse
 import sys
 
 parser = argparse.ArgumentParser(prog="2do", description='Recursively read .2do files')
-parser.add_argument("-f", help="The tasklist from to draw tasks from or add tasks to.", nargs='?', default=".2do")
+parser.add_argument("path", help="The tasklist from to draw tasks from or add tasks to.", nargs='?', default=".2do")
 parser.add_argument("-i", help="Inhibit recursive crawling for tasks",
                     action="store_false", dest="recurse")
 args = parser.parse_args()
 
-rootContext = lib2do.taskContext(args.f)
+rootContext = lib2do.taskContext(args.path)
 for task in rootContext.allTasks():
     print task.text
