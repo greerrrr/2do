90de0faba94517c0a20598acc6c4a26ecb83c965
Rob Greer <rob.g.greer@gmail.com>
2012-03-07 00:45:01 -0500

    Improved file recognization, began staging for root searching.

--- 2do-cli.py
+++ 2do-cli.py
@@ -4,6 +4,24 @@ import argparse
 import sys
 import os
 
+def find_root(filelist):
+    adjacencylist = dict()
+    for todo in filelist:
+        adjacent = []
+        try:
+            f = open(todo, "r")
+        except:
+            print "Failed to open %s when finding root."
+            break
+
+        for line in f:
+            if line.strip()[0:2] == "./":
+                adjacent.append(os.path.basename(line.strip()))
+        adjacencylist[todo] = adjacent
+
+
+
+
 parser = argparse.ArgumentParser(prog="2do", description='Recursively read .2do files')
 parser.add_argument("path", help="The tasklist from to draw tasks from or add tasks to.", nargs='?')
 parser.add_argument("-i", help="Inhibit recursive crawling for tasks",
@@ -17,11 +35,20 @@ if filepath is None:
     for filename in os.listdir("."):
         if filename[-4:] == ".2do":
             candidates[intkey] = filename
-    print "No file given. Please select one of:"
-    for key in candidates:
-        print key,":", candidates[key]
-    selection = input("Select file [1-%i]:"%key)
-    filepath = candidates[selection]
+            intkey+=1
+    if intkey == 1:
+        filepath = candidates[0]
+    else:
+        print "No file given. Please select one of:"
+        for key in candidates:
+            print key,":", candidates[key]
+        selection = " "
+        while selection != "" and selection.isdigit() == False:
+            selection = raw_input("Select file [0-%i]:"%key)
+            if selection == "":
+                selection = "0"
+        selection = int(selection)
+        filepath = candidates[selection]
 
 rootContext = lib2do.taskContext(filepath)
 for task in rootContext.allTasks():
